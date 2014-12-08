4c9a0db656daef206392797f12063bdb985cf600
Robin Radic <robin@radic.nl>
2014-12-08 05:54:08 +0100

    fixed bug in storage.set which prevented expires

--- src/storage.js
+++ src/storage.js
@@ -20,7 +20,7 @@ define([
         }
         if(options.expires){
             var now = Math.floor((Date.now() / 1000) / 60);
-            window['localStorage'].setItem(key + ':expire', now + options.expire);
+            window['localStorage'].setItem(key + ':expire', now + options.expires);
         }
         window['localStorage'].setItem(key, val);
     };
